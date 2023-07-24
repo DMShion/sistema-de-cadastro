f45e1922a8e94dd46b9cf5e0859005acc80ff85e
Diego Shion <dm.diego@gmail.com>
2023-07-24 14:56:19 -0300

    restaurando versão

--- app.js
+++ app.js
@@ -2,5 +2,3 @@ console.log ("Rodando o sistema de gerencimaneto de jogos")
 console.log ("A aplicação está se conectando no banco de dados")
 //adicionando mais uma linha
 console.log ("Diego adicionou outra linha no app.js")
-
-console.log ("Mais uma adição local")
