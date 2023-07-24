c62de7137c02c733d6d7c25c16968bb3f8d31295
Diego Shion <dm.diego@gmail.com>
2023-07-24 14:50:39 -0300

    Adicionando outra linha no console

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log ("Rodando o sistema de gerencimaneto de jogos")
 console.log ("A aplicação está se conectando no banco de dados")
 //adicionando mais uma linha
 console.log ("Diego adicionou outra linha no app.js")
+
+console.log ("Mais uma adição local")
