009584a61e41ea70e87b88f734da58cf5d2836f0
DMShion <dm.diego@gmail.com>
2023-07-24 14:45:44 -0300

    Adição de linha por - Diego

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log ("Rodando o sistema de gerencimaneto de jogos")
 console.log ("A aplicação está se conectando no banco de dados")
+//adicionando mais uma linha
+console.log ("Diego adicionou outra linha no app.js")
